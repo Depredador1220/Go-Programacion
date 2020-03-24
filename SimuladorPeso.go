07a2d4a91c9dd2c8a75aef1c753477f98e6daa3e
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-03-23 22:25:59 -0600

    Create SimuladorPeso.go

---
+++ SimuladorPeso.go
@@ -0,0 +1,14 @@
+//Simulador de peso en marte y la tierra
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Mi peso en la superficie de marte es: ")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print(" kilos, y deberia tener: ")
+	fmt.Print(41 * 365 / 687)
+	fmt.Print(" años de edad")
+}
