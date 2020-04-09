371d5edac506ddda05d31f449c59fa20ed27e6c1
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-08 19:23:30 -0500

    Create OperadorNot.go

---
+++ OperadorNot.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	var tieneAntorcha = true
+	var antorchaFuego = false
+	
+	if !tieneAntorcha || !antorchaFuego {
+		fmt.Println("Esta oscuro")
+	} 
+}
