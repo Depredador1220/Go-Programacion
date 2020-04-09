3d4b001c912bb55657c571c06d5595346908293a
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-08 19:12:11 -0500

    Create UsandoIfElse.go

---
+++ UsandoIfElse.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var comando = "Ve al este"
+	
+	if comando == "Ve al este"{
+		fmt.Println("Tu entraras en una cueva donde permaneceras el resto de tu vida")
+	} else {
+		fmt.Println("No pasara nada")
+	}
+}
