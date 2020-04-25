d114125e184b67737b4c92b0110a883161cbd776
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-25 13:36:54 -0500

    Create cicloFor.go

---
+++ cicloFor.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var contador = 0;
+	
+	for contador = 10; contador > 0; contador-- {
+		fmt.Println(contador)
+	} 
+	
+	fmt.Println(contador)
+}
